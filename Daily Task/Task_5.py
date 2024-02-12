3266d727693388913e6e6de54faf9917b7ae22e3
Parvathy krishna <95429804+Parvathykrishna1@users.noreply.github.com>
2024-02-12 15:10:15 +0530

    Initial commit

--- Daily Task/Task_5.py
+++ Daily Task/Task_5.py
@@ -1,5 +1,5 @@
 FUNCTIONS – Task-5
-1.	Write a Python function that calculates the factorial of a given integer. Demonstrate how to call this function with an example.
+1.Write a Python function that calculates the factorial of a given integer. Demonstrate how to call this function with an example.
 def factorial(n):
     if n == 0:
         return 1
@@ -11,7 +11,7 @@ fact = factorial(n)
 print(f"The factorial of {n} is {fact}")
   
     
-2.	Create a function that takes two arguments, a name and an age, and prints a message like "Hello,   [Name]! You are [Age] years old." Call this function with your name and age as arguments.
+2.Create a function that takes two arguments, a name and an age, and prints a message like "Hello,   [Name]! You are [Age] years old." Call this function with your name and age as arguments.
 def f(name,age):
     print(f"Hello, {name}! You are {age} years old.")
 
@@ -19,7 +19,7 @@ name=input("Enter name:")
 age=int(input("Enter age:"))
 f(name,age)
 
-3.	Write a Python function to find the maximum of three numbers.
+3.Write a Python function to find the maximum of three numbers.
 def greatestnum(a,b,c):
     return max(a,b,c)
 
@@ -30,8 +30,7 @@ max=greatestnum(a,b,c)
 print(f"Maximum of three numbers is {max}")
 
 
-
-4.	Write a Python program to reverse a string
+4.Write a Python program to reverse a string
 def reverse_string(s):
        return s[::-1]
 
@@ -39,7 +38,8 @@ s=input("Enter the string:")
 rev=reverse_string(s)
 print(f"Reverse of {s} is {rev}")
 
-5.	Write a Python function that accepts a string and counts the number of upper and lower case letters.
+
+5.Write a Python function that accepts a string and counts the number of upper and lower case letters.
 def letters_count(s):
     upper_count = 0
     lower_count = 0
@@ -54,8 +54,7 @@ s = input("Enter a string: ")
 letters_count(s)
 
 
-
-6.	Write a Python function that takes a number as a parameter and checks whether the number is prime or not. 
+6.Write a Python function that takes a number as a parameter and checks whether the number is prime or not. 
 def primenumber(n):
     "Prime number or not"
     for i in range(2,n):
@@ -68,8 +67,9 @@ def primenumber(n):
 
 a=int(input("Enter a number"))
 primenumber(a)
-           
-7.	Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically.
+
+
+7.Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically.
 Sample Items : green-red-yellow-black-white
 Expected Result : black-green-red-white-yellow
 def str_sort(string):
@@ -82,7 +82,8 @@ s = input('Enter a sequence of colors separated with hyphen: ')
 result = str_sort(s)
 print("Sorted sequence:", result)
 
- 8.	Define a function that accepts 2 values and return its sum, subtraction and multiplication. Using 4 types of functions based on arguments and return type.
+
+ 8.Define a function that accepts 2 values and return its sum, subtraction and multiplication. Using 4 types of functions based on arguments and return type.
 def calculator(a, b):
     return a + b, a - b, a * b
 
@@ -104,7 +105,7 @@ print("Subtraction:", subtraction_result)
 print("Multiplication:", multiplication_result)
 
 
- 9.	Define a function that accepts roll number and returns whether the student is present or absent.
+9.Define a function that accepts roll number and returns whether the student is present or absent.
 def student_attnd(roll_no):
     roll_list = [26,24,55,72,36]
     if roll_no in roll_list:
@@ -116,7 +117,7 @@ roll_no = int(input("Enter your roll number:"))
 student_attnd(roll_no)
 
 
- 10.	Define a function that accepts lowercase words and returns uppercase words.
+10.Define a function that accepts lowercase words and returns uppercase words.
 def to_uppercase(str):
  if str.islower():
   return str.upper()
