b8b06f9e32634d642c3db83168a8fdc759fe8b16
Parvathy krishna <95429804+Parvathykrishna1@users.noreply.github.com>
2024-02-14 13:29:50 +0530

    Task_7.py

--- Daily Task/Task_7.py
+++ Daily Task/Task_7.py
@@ -1,20 +1,22 @@
-# 1.Write a Python program to calculate the length of a string.
-# s=input("Enter a string:")
-# l=len(s)
-# print("Given string:",s)
-# print("Length of the given string:",l)
-# 2.Print even length words in a string.
-# Sample String : ''I love coding"
-# Expected Result : “love, coding”
+1.Write a Python program to calculate the length of a string.
+s=input("Enter a string:")
+l=len(s)
+print("Given string:",s)
+print("Length of the given string:",l)
 
-# s="I love coding"
-# str = s.split(" ")
-# l=list(filter(lambda x: (len(x)%2==0),str))
-# print(" ".join(l))
 
-# 3.Write a Python code to remove all characters except a           
-# Sample String : 'exercises'
-# Expected Result : 'eee' (Removed all characters except special character : e)
+2.Print even length words in a string.
+Sample String : ''I love coding"
+Expected Result : “love, coding”
+
+s="I love coding"
+str = s.split(" ")
+l=list(filter(lambda x: (len(x)%2==0),str))
+print(" ".join(l))
+
+3.Write a Python code to remove all characters except a           
+Sample String : 'exercises'
+Expected Result : 'eee' (Removed all characters except special character : e)
 def remove_characters(str,n):
   return ''.join([x for x in str if x == n])
 s = "exercises"
