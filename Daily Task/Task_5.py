a685bbc07a4b3282dfd6936522b7726321baa16a
Parvathy krishna <95429804+Parvathykrishna1@users.noreply.github.com>
2024-02-15 18:02:59 +0530

    commit

--- Daily Task/Task_5.py
+++ Daily Task/Task_5.py
@@ -8,15 +8,15 @@ def factorial(n):
 
 n = int(input("Enter a number:"))
 fact = factorial(n)
-print(f"The factorial of {n} is {fact}")
+print("The factorial of ",n," is ",fact,)
   
     
 2.Create a function that takes two arguments, a name and an age, and prints a message like "Hello,   [Name]! You are [Age] years old." Call this function with your name and age as arguments.
-def f(name,age):
-    print(f"Hello, {name}! You are {age} years old.")
+def f(n,a):
+    print("Hello,",n,"! You are",a,"years old.")
 
-name=input("Enter name:")
-age=int(input("Enter age:"))
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
 f(name,age)
 
 3.Write a Python function to find the maximum of three numbers.
@@ -26,8 +26,8 @@ def greatestnum(a,b,c):
 a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 c=int(input("Enter third number:"))
-max=greatestnum(a,b,c)
-print(f"Maximum of three numbers is {max}")
+maxnum=greatestnum(a,b,c)
+print("Maximum of three numbers is",maxnum)
 
 
 4.Write a Python program to reverse a string
@@ -36,7 +36,7 @@ def reverse_string(s):
 
 s=input("Enter the string:")
 rev=reverse_string(s)
-print(f"Reverse of {s} is {rev}")
+print("Reverse of",s,"is",rev)
 
 
 5.Write a Python function that accepts a string and counts the number of upper and lower case letters.
@@ -62,7 +62,7 @@ def primenumber(n):
               print("Given Number is not a prime number")
               break
     else:
-        print("prime number")
+        print("Given number is a prime number")
     return
 
 a=int(input("Enter a number"))
@@ -72,15 +72,15 @@ primenumber(a)
 7.Write a Python program that accepts a hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically.
 Sample Items : green-red-yellow-black-white
 Expected Result : black-green-red-white-yellow
-def str_sort(string):
-  b = string.split('-')
-  b.sort()
-  c = '-'.join(b)
-  return c
+def str_sort(s):
+  s1 = s.split('-')
+  s1.sort()
+  s2 = '-'.join(s1)
+  return s2
 
-s = input('Enter a sequence of colors separated with hyphen: ')
-result = str_sort(s)
-print("Sorted sequence:", result)
+str = input('Enter a hyphen-separated sequence of words: ')
+final_sequence = str_sort(str)
+print("Sorted sequence:", final_sequence)
 
 
  8.Define a function that accepts 2 values and return its sum, subtraction and multiplication. Using 4 types of functions based on arguments and return type.
@@ -96,25 +96,25 @@ def subtraction(a, b):
 def multiplication(a, b):
     return a * b
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
 
-sum_result, subtraction_result, multiplication_result = calculator(num1, num2)
-print("Sum:", sum_result)
-print("Subtraction:", subtraction_result)
-print("Multiplication:", multiplication_result)
+add, sub, mult = calculator(n1, n2)
+print("Sum:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mult)
 
 
 9.Define a function that accepts roll number and returns whether the student is present or absent.
-def student_attnd(roll_no):
-    roll_list = [26,24,55,72,36]
-    if roll_no in roll_list:
+def student_attend(roll_no):
+    present_list = [22,23,24,55,60,72]
+    if roll_no in present_list:
      print("Present")
     else:
      print("Absent")
 
 roll_no = int(input("Enter your roll number:"))
-student_attnd(roll_no)
+student_attend(roll_no)
 
 
 10.Define a function that accepts lowercase words and returns uppercase words.
