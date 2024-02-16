8c5dc9057b41cfa558101f0a7a3f59ce4471265a
Parvathy krishna <95429804+Parvathykrishna1@users.noreply.github.com>
2024-02-16 13:42:24 +0530

    commit

--- Daily Task/Task_7.py
+++ Daily Task/Task_7.py
@@ -1,30 +1,34 @@
-1.Write a Python program to calculate the length of a string.
-s=input("Enter a string:")
-l=len(s)
-print("Given string:",s)
-print("Length of the given string:",l)
+# 1.Write a Python program to calculate the length of a string.
+# s=input("Enter a string:")
+# l=len(s)
+# print("Given string:",s)
+# print("Length of the given string:",l)
 
 
-2.Print even length words in a string.
-Sample String : ''I love coding"
-Expected Result : “love, coding”
+# 2.Print even length words in a string.
+# Sample String : ''I love coding"
+# Expected Result : “love, coding”
 
-s = input("Enter a string: ")
-words = s.split()
+# s = input("Enter a string: ")
+# words = s.split()
 
-for i in words:
-    if len(i)%2==0:
-        print("Even length words in the string:",i)
+# even_words = [x for x in words if len(x)% 2 == 0]
 
-3.Write a Python code to remove all characters except a           
-Sample String : 'exercises'
-Expected Result : 'eee' (Removed all characters except special character : e)
+# if even_words:
+#     print("Even length words in the string:", ", ".join(even_words))
+# else:
+#     print("No even length word in the string.")
+        
 
-s = input("Enter a string:")
-except_char = "a"
+# 3.Write a Python code to remove all characters except a           
+# Sample String : 'exercises'
+# Expected Result : 'eee' (Removed all characters except special character : e)
 
-print("Given string:", s)
-print(f"Remove all characters except {except_char} in the given string:")
-str = ''.join([i for i in s if i == except_char])
-print(str)
+# s = input("Enter a string:")
+# except_char = "a"
+
+# print("Given string:", s)
+# print(f"Remove all characters except {except_char} in the given string:")
+# str = ''.join([i for i in s if i == except_char])
+# print(str)
 
