70ce4c87dd339db1fa730a9a034dbb7ff39dafe7
Parvathykrishna1 <parvathyk285@gmail.com>
2024-03-18 09:45:39 +0000

    Mar 18, 2024, 3:15 PM

--- myproject/app1/views.py
+++ myproject/app1/views.py
@@ -17,7 +17,7 @@ def login(request):
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
-            return redirect(request, 'login.html', )
+            return redirect(''
 
 
 def category_form(request):
