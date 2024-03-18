e178f15696df1fc05da6fbd1b3529ae8b762de6b
Parvathykrishna1 <parvathyk285@gmail.com>
2024-03-18 10:31:54 +0000

    Mar 18, 2024, 4:01 PM

--- myproject/app1/urls.py
+++ myproject/app1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/',views.user_login),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/',views.user_logout),
     path('home/',views.home,name='home'),
     path('addcategory/',views.category_form,name='addcategory'),
     path('category_list/', views.category_list, name='category_list'),
